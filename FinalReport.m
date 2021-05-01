ff21a550c2b966d11fb592eeca51387d63198495
GungsarYungtang <gYungtang2@huskers.unl.edu>
2021-05-01 15:16:45 -0500

    added final report

---
+++ FinalReport.m
@@ -0,0 +1,6 @@
+Initially for this project i planned to include all the number keys and make the calculation only using the buttons and 
+it was approved by LA Miguel Moreno. But I decided to use the edit boxes to type in the input instead of the number buttons and changed the 
+the buttons for calculation type from push button to radio button. And it was approved by LA Ryan Martz. In this project I had some struggles 
+in using the buttongroups but i was able to write all the other parts without help. For the buttongroups I went to LA office hours and
+Emme Campbell helped me to make finishing touches. In the future I am hoping to use gui for class or for work but I am not sure what i will use 
+it for yet.
