14850ecfcc31547a7d84f3cb239dea36c17d2644
GungsarYungtang <gYungtang2@huskers.unl.edu>
2021-05-01 15:25:54 -0500

    added comments to the calculator code

--- Calculator.m
+++ Calculator.m
@@ -1,21 +1,20 @@
  function [] = Calculator()
     
     close all;
-    global gg;
-    gg.data = 0;
-    
+    global gg; %creat gui
+    %make the figure
     gg.fig = figure('numbertitle','off','name','Calculator');
-    
+    %clear and enter buttons
     gg.clear = uicontrol('style','pushbutton','units','normalized','position',[.80 .520 .10 .20],'string','C','callback', {@clear});
     gg.enter = uicontrol('style','pushbutton','units','normalized','position',[.80 .280 .10 .20],'string','Enter','callback', {@enter});
-     
+     %input and output buttons and texts indicating the button
     gg.input1 = uicontrol('style','edit','units','normalized','position',[.10 .780 .4 0.1],'string','','horizontalalignment','left','fontsize',15); 
     gg.input1str = uicontrol('style','text','units','normalized','position',[.10 .90 .3 0.05],'string','INPUT1:','horizontalalignment','left','fontsize',15); 
     gg.input2 = uicontrol('style','edit','units','normalized','position',[.10 .50 .4 0.1],'string','','horizontalalignment','left','fontsize',15);
     gg.input2str = uicontrol('style','text','units','normalized','position',[.10 .620 .3 0.05],'string','INPUT2:','horizontalalignment','left','fontsize',15); 
     gg.output = uicontrol('style','text','units','normalized','position',[.10 .20 .4 0.1],'string','','horizontalalignment','left','fontsize',15);
     gg.outputstr = uicontrol('style','text','units','normalized','position',[.10 .32 .3 0.05],'string','SOLUTION:','horizontalalignment','left','fontsize',15); 
-    
+    %all the operation buttons and they are all in the buttongroup
     operationsButtons = uibuttongroup('FontSize',12,'Position',[.55 .270 .20 .62],'SelectionChangedFcn',{@buttongroup});
     gg.add = uicontrol(operationsButtons,'style','radio','units','normalized','position',[.3 .80 .50 .10],'string','+','horizontalalignment','left','fontsize',20);
     gg.subtract = uicontrol(operationsButtons,'style','radio','units','normalized','position',[.3 .60 .50 .10],'string','-','horizontalalignment','left','fontsize',20);
@@ -24,7 +23,7 @@
          
  end
 
- 
+ %call back for  the operation buttongroups to indicate selected operation
  function [] = buttongroup(source,event)
     global mat;
     if strcmp('+',event.NewValue.String)
@@ -39,7 +38,8 @@
         
  
  end
-
+% call back for the enter button that make all calculation and display the answer
+%using the data from button group that is stored gui element mat.
 function [] = enter(~,~)
     global gg;
     global mat;
@@ -66,7 +66,8 @@ function [] = enter(~,~)
     
     end
 end
-
+%call back for clear button that reset all the display boxes and the
+%solution to blank.
 function[] = clear(~,~)
     global gg;
     gg.input1.String = '' ;
